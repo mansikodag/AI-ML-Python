0b31606fa523b4fd69f432a5d25c0c52bef5e27b
mansikodag <90202649+mansikodag@users.noreply.github.com>
2021-09-06 23:04:52 +0530

    Create list.py

---
+++ Python/list.py
@@ -0,0 +1,25 @@
+#list
+characters=['a','b','c','m','l','n','s','q','p','k','d','f','g']
+print(characters)#to print characters
+print(characters[2:5])#to print characters at 2,5
+characters.reverse()#to reverse the list
+print(characters)
+characters.reverse()
+characters.append('h')#to append character in list(by def at end)
+print(characters)
+characters.insert(3,'i')#to insert character('i') at 3
+print(characters)
+characters.sort()#to sort the list
+print(characters)
+characters.remove('f')#to remove character from list
+print(characters)
+characters.pop()#remove and return character(by def end)
+print(characters)
+characters.clear()#to clear all the characters in the list
+print(characters)
+
+#tuppel
+tuple=['a','b','c','d','e','f','g','h','i']
+print(tuple)
+print(tuple.index('f'))#to find index of 'f'
+print(tuple.count('i'))#to count no of 'i'
