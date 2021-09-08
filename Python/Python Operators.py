dfc87e70760d2dba85410f3dde8e17bd653fa8c6
mansikodag <90202649+mansikodag@users.noreply.github.com>
2021-09-08 13:33:37 +0530

    Create Python Operators.py

---
+++ Python/Python Operators.py
@@ -0,0 +1,69 @@
+#Arithmetic Operators
+a=int(input("Enter First Number:\n"))
+b=int(input("Enter second Number:\n"))
+print("\nAddition of",a,"and",b,"is",a+b,".")
+print("\nSubstraction of",a,"and",b,"is",a-b,"." )
+print("\nMultiplication of",a,"and",b,"is",a*b,".")
+print("\nDivision of",a,"and",b,"is",a/b,".")
+print("\nModulas of",a,"and",b,"is",a%b,".")
+print("\nfloor Division of",a,"and",b,"is",a//b)
+print("\nExponent of",a,"of",b,"is",a**b)
+
+
+#Relational Operators
+a=int(input("\nEnter First Number:\n"))
+b=int(input("Enter second Number:\n"))
+print("result of != operation is:",a!=b)
+print("result of == operation is:",a==b)
+print("result of < operation is:",a<b)
+print("result of > operation is:",a>b)
+print("result of >= operation is:",a>=b)
+print("result of <= operation is:",a<=b)
+
+
+#Unary Operators
+a=int(input("\nEnter First Number:\n"))
+b=int(input("Enter second Number:\n"))
+a=+a
+b=-b
+print("Result of unary +",a)
+print("Result of unary -",b)
+
+
+#Assignment Operators
+a=int(input("\nEnter First Number:\n"))
+b=int(input("Enter second Number:\n"))
+a=a+1
+b=b-1
+print("REsult of assignment + operator",a)
+print("REsult of assignment - operator",b)
+
+
+
+'''Bit-wise Operators
+a=int(input("\nEnter First Number:\n"))
+b=int(input("Enter second Number:\n"))
+'''
+
+
+#Logical Operators
+a=int(input("\nEnter First Number:\n"))
+b=int(input("Enter second Number:\n"))
+c=int(input("Enter third Number:\n"))
+
+
+#and operator
+output=a<b and b<c
+print("Result of and operator is:",output)
+
+
+#or operator
+output=a>b or a<b
+print("Result of or operator is:",output)
+
+
+#not operator
+not a
+not b
+not c
+print("Result of not operator is","a=",a,"b=","b=",b,"c=",c)
